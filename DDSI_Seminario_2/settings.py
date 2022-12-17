cf0c888dfabf86518f795392347ebda4c4adc59d
BetulioMay <cesarmay@protonmail.com>
2022-12-17 22:30:44 +0100

    Tutorial parte 7

--- DDSI_Seminario_2/settings.py
+++ DDSI_Seminario_2/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "DDSI_Seminario_2.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
